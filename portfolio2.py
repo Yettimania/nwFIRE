765654c8368bf34078a649e8bc4867aa39f06f49
Yettimania <kylesnyder42@gmail.com>
2021-01-21 16:37:57 -0800

    Add show and detail

--- portfolio2.py
+++ portfolio2.py
@@ -44,11 +44,40 @@ class Portfolio:
             x.ticker = asset['ticker'] 
             self.assets.append(x)
 
-#kyle = Portfolio()
+    def summary(self):
+        dash = '-' * 30
+        print(dash)
+        print('{:<10s}{:>4s}{:>12s}'.format('ASSET','CATEGORY','AMOUNT'))
+        print(dash)
+        for obj in self.assets:
+            asset = obj.name
+            category = obj.category
+            amount = obj.amount
+            print('{:<10s}{:<15s}{:>1f}'.format(asset,category,amount))
+    
+    def detail(self,name):
+        for obj in self.assets:
+            if obj.name == name:
+                print("SUMMARY OF ASSET:")
+                print(f'Name: {obj.name}\nCategory: {obj.category}\nAmount: {obj.amount}\n\
+MStar: {obj.mstar}\nComposition: (c/b/s): {obj.composition}\nTicker: {obj.ticker}')
+
+
+    @property
+    def asset_list(self):
+        asset_list = []
+        for obj in self.assets:
+            asset_list.append(obj.name)
+        return asset_list
+
+kyle = Portfolio()
 #kyle.add('LLL')
 #kyle.add('FLS')
 #
 #kyle.save()
-#kyle.load()
+kyle.load()
+kyle.summary()
+kyle.detail('FLS')
+print(kyle.asset_list)
 #for obj in kyle.assets:
 #    print(obj.__dict__)
